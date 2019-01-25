e2c7e7c4a8932a650b4b3cf66c2b0c62f2544b5d
Len-art <lenart.velkavrh@gmail.com>
2019-01-25 17:08:36 +0100

    cleanup and lint

--- api/example.js
+++ api/example.js
@@ -1,9 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
-import {
-  API_VERSION,
-  BACKEND_URL,
-} from 'env';
+import { API_VERSION, BACKEND_URL } from 'env'
 
 export const fetchRandomFlower = async () => {
   try {
@@ -11,9 +8,9 @@ export const fetchRandomFlower = async () => {
       method: 'POST',
       url: `${BACKEND_URL}/api/${API_VERSION}/flowers/random`,
       responseType: 'json',
-    });
-    return data.json();
+    })
+    return data.json()
   } catch (err) {
-    return err.response;
+    return err.response
   }
-};
+}
