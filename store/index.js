ce8478938cb6578326a2b3cabd05e6b31852d56b
len-art <lenart.velkavrh@gmail.com>
2019-02-02 10:46:08 +0100

    handle refresh tokens

--- store/index.js
+++ store/index.js
@@ -1,19 +1,13 @@
 import { action, observable } from 'mobx'
 import axios from 'axios'
+import { init } from '@sentry/browser'
 import { config } from '../api'
-/*
-res.json({
-  user: {
-    name: user.name,
-    email,
-    createdAt,
-    lastLoginAt,
-  },
-  token,
-}) */
+import { tokenHelper } from '../helpers'
+
 export default class {
   constructor() {
     this.client = axios.create(config)
+    this.init()
   }
 
   @observable
@@ -28,29 +22,73 @@ export default class {
   @observable
   selectedMonth = 0
 
-  @action
-  async handleLogin({ email, password, withData }) {
+  async init() {
+    if (typeof window === 'undefined') return
+    const accessToken = sessionStorage.getItem('accessToken')
+    if (accessToken && tokenHelper.isValid(accessToken)) {
+      this.client.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+      this.getUserData()
+    } else {
+      const refreshToken = localStorage.getItem('refreshToken')
+      const refreshSecret = localStorage.getItem('refreshSecret')
+      if (refreshToken && refreshSecret) {
+        this.getToken({ refreshToken, refreshSecret })
+      }
+    }
+  }
+
+  async getUserData() {
+    try {
+      // call api
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async getToken({ refreshToken, refreshSecret }) {
+    try {
+      console.log('gettingtoken')
+      const { data } = await this.client.get('users/token', { refreshToken, refreshSecret })
+      this.handleLoginSuccess(data)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  async handleLogin({ email, password }) {
     email = 'lenart.velkavrh@gmail.com'
     password = 'foo'
     try {
       const { data } = await this.client.post('users/login', {
         email,
         password,
-        withData,
       })
-      const {
-        user, token, projects, month,
-      } = data
-      this.user = user
-      this.setToken(token)
-      if (withData) {
-        this.projects = projects
-        this.month = month
-      }
+      this.handleLoginSuccess(data)
       return true
     } catch (error) {
       console.error(error)
       return false
+    }
+  }
+
+  @action
+  handleLoginSuccess({
+    user, accessToken, refreshToken, refreshSecret,
+  }) {
+    if (user) {
+      this.user = user
+    }
+    if (accessToken) {
+      sessionStorage.setItem('accessToken', accessToken)
+      this.client.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+    }
+    if (refreshToken) {
+      localStorage.setItem('refreshToken', refresh)
+    }
+    if (refreshSecret) {
+      localStorage.setItem('refreshSecret', secret)
     }
   }
 
@@ -68,13 +106,6 @@ export default class {
     } catch (error) {
       console.error(error)
       return false
-    }
-  }
-
-  setToken(token) {
-    if (token) {
-      localStorage.setItem('token', token)
-      this.client.defaults.headers.common.Authorization = `Bearer ${token}`
     }
   }
 
