a416f4ca1e21db010f32541120b9450c975fc36f
Lenart Velkavrh <lenart.velkavrh@poviolabs.com>
2019-02-07 18:06:45 +0100

    add missing style to modal

--- components/modal/index.js
+++ components/modal/index.js
@@ -42,6 +42,7 @@ class Modal extends React.Component {
               outline: none;
             }
             .content {
+              position: relative;
               padding: 20px;
               border-radius: 5px;
               background-color: #fff;
