207f7b6141578363199a319bd92d20163da04c79
leon <kolencleon@gmail.com>
2019-02-11 12:10:35 +0100

    logout button resets cookies and clears window

--- components/header/user.js
+++ components/header/user.js
@@ -2,7 +2,32 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import Link from 'next/link'
 
-const LoggedIn = ({ user: { name } }) => <div>{`Hey, ${name}!`}</div>
+const LoggedIn = ({ user: { name }, resetCookies }) => (
+  <div>
+    {`Hey, ${name}!`}
+    <button onClick={resetCookies} className="logout">
+      LogOut
+      <style jsx>
+        {`
+          .logout {
+            margin: 10px;
+            padding: 2px 9px;
+            border-style: none;
+            border-radius: 4px;
+            background-color: transparent;
+            font-size: 10px;
+            font-style: italic;
+            opacity: 0.5;
+          }
+          .logout:hover {
+            opacity: 1;
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </button>
+  </div>
+)
 
 const NotLoggedIn = () => (
   <div>
@@ -13,7 +38,9 @@ const NotLoggedIn = () => (
 )
 
 export default inject('store')(
-  observer(({ store: { auth: { user } } }) => (
-    <div className="user">{user ? <LoggedIn user={user} /> : <NotLoggedIn />}</div>
+  observer(({ store: { auth: { user, resetCookies } } }) => (
+    <div className="user">
+      {user ? <LoggedIn user={user} resetCookies={resetCookies} /> : <NotLoggedIn />}
+    </div>
   )),
 )
