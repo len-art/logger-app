52e4c06f904dcdbefc5c87e8d92bc002ca52930b
leon <kolencleon@gmail.com>
2019-02-11 15:12:38 +0100

    addMonth work in progress

--- components/footer/addMonth.js
+++ components/footer/addMonth.js
@@ -3,8 +3,13 @@ import React from 'react'
 import Fab from '../fab'
 
 class AddMonth extends React.Component {
+  createMonth = () => {
+    console.log('callMonthsCreate')
+  }
+
   render() {
-    return <Fab />
+    // create another method for true/false current month. if current month false(doesn't exist) than return plus button, else empty component
+    return <Fab onClick={this.createMonth} />
   }
 }
 
