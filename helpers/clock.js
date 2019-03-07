d366eddda5ddf762bcbfcb7caa40c1b9490229b8
Lenart Velkavrh <lenart.velkavrh@poviolabs.com>
2019-03-07 19:26:45 +0100

    cleanup

--- helpers/clock.js
+++ helpers/clock.js
@@ -14,17 +14,10 @@ const createMinutes = () => Array.from(new Array(12), (_, i) => {
   return { m, deg }
 })
 
-// TODO: this is bad and i should feel bad fix it to be more elegant
-const getHourFromDegrees = (deg) => {
-  const value = Math.round((deg / 360) * 12)
-  return value === 0 ? 12 : value
-}
-
 const getMinuteFromDegrees = deg => Math.round((deg / 360) * 60) % 60
 
 export default {
   createHours,
   createMinutes,
-  getHourFromDegrees,
   getMinuteFromDegrees,
 }
