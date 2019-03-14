32fc579b3b23abfd08fa902903eaf7815844303d
leon <kolencleon@gmail.com>
2019-03-14 13:08:29 +0100

    isSameMonth not working properly

--- components/footer/addMonth.js
+++ components/footer/addMonth.js
@@ -1,42 +1,36 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-import format from 'date-fns/format'
+import startOfMonth from 'date-fns/startOfMonth'
+import isSameMonth from 'date-fns/isSameMonth'
 import Fab from '../fab'
 
 @inject('store')
 @observer
 class AddMonth extends React.Component {
-  createMonth = () => {
-    const projectId = this.props.store.selectedProject
-    const dayInMonth = new Date()
-    this.props.store.createMonth(projectId, dayInMonth)
-    console.log(this.props.store.selectedMonth)
-    // try {
-    //   this.error = undefined
-    //   this.isLoading = true
-    //   const projectId = this.props.store.selectedProject
-    //   const dayInMonth = new Date()
-    //   await this.props.store.createMonth({ projectId, dayInMonth })
-    // } catch (error) {
-    //   console.error(error)
-    //   if (error.response.data) {
-    //     this.error = error.response.data
-    //   }
-    // } finally {
-    //   this.isLoading = false
-    // }
-  }
+  // createMonth = () => {
+  //   const projectId = this.props.store.selectedProject
+  //   const dayInMonth = new Date()
+  //   this.props.store.createMonth(projectId, dayInMonth)
+  //   console.log(this.props.store.months)
+  // }
 
   checkIsSameMonth = () => {
     const dayInMonth = new Date()
-    props.store.months.some((month) => {
-      month.startsAt === dayInMonth.startsAt
-    })
+    console.log(
+      this.props.store.months.some((month) => {
+        console.log(dayInMonth)
+        console.log(month.startsAt)
+        isSameMonth(
+          new Date('Thu Mar 14 2019 12:58:33 GMT+0100 (Central European Standard Time)'),
+          new Date('Fri Mar 01 2019 01:00:00 GMT+0100 (Central European Standard Time)'),
+        )
+      }),
+    )
   }
 
   render() {
     // create another method for true/false current month. if current month false(doesn't exist) than return plus button, else empty component
-    return <Fab onClick={this.createMonth} />
+    return <Fab onClick={this.checkIsSameMonth} />
   }
 }
 
